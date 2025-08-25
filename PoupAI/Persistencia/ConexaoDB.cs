151b32773c4912168e6845338512438ab760e104
bertanj <mateusbertani@edu.unifil.br>
2025-08-25 17:25:00 -0300

    alterações Dapper

---
+++ PoupAI/Persistencia/ConexaoDB.cs
@@ -0,0 +1,33 @@
+﻿using Dapper.Contrib.Extensions;
+using Npgsql;
+
+namespace PoupAI.Persistencia
+{
+    public class ConexaoDB
+    {
+        private static readonly string stringConexao;
+
+        static ConexaoDB()
+        {
+            var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            stringConexao = config.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("String de conexão 'DefaultConnection' não encontrada.");
+
+            SqlMapperExtensions.TableNameMapper = (type) =>
+            {
+                return type.Name;
+            };
+        }
+
+        // Fechar conexão após uso
+        public static NpgsqlConnection ObterConexao()
+        {
+            var conexao = new NpgsqlConnection(stringConexao);
+            conexao.Open();
+            return conexao;
+        }
+    }
+}
---
+++ PoupAI/Repositories/ReceitaRepository.cs
@@ -0,0 +1,8 @@
+using PoupAI.Persistencia;
+using Dapper.Contrib.Extensions;
+using Api.Comum;
+using PoupAI.Interfaces;
+
+public class ReceitaRepository : IRepository<Receita>
+    
+}
